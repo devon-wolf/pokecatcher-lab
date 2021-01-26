ec37642b783b22b532b73e6e8163aefe77ef2cf0
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-26 13:35:30 -0800

    Add stacked bar to results chart

--- results/munge-utils.js
+++ results/munge-utils.js
@@ -1,5 +1,3 @@
-// combined the seen, caught, and label munging into one function with a paramter to indicate which property should be accessed
-
 export function makePropertyArray(arrayOfObjects, propertyString) {
     const propertyArray = [];
     for (let item of arrayOfObjects) {
@@ -7,19 +5,3 @@ export function makePropertyArray(arrayOfObjects, propertyString) {
     }
     return propertyArray;
 }
-
-// export function makeSeenArray(arrayOfObjects) {
-//     const seenArray = [];
-//     for (let item of arrayOfObjects) {
-//         seenArray.push(item.seen);
-//     }
-//     return seenArray;
-// }
-
-// export function makeLabelArray(arrayOfObjects) {
-//     const labelArray = [];
-//     for (let item of arrayOfObjects) {
-//         labelArray.push(item.pokemon); // or whatever the name property is called in that array
-//     }
-//     return labelArray;
-// }
--- results/results.js
+++ results/results.js
@@ -1,11 +1,10 @@
-// import { generateTableRows } from './render-results-utils.js';
 import { clearStats, getStats } from '../data-utils.js';
 import { makePropertyArray } from './munge-utils.js';
 import pokemon from '../data/data.js';
 
 const resetButton = document.getElementById('reset-button');
 const pokeStats = getStats();
-const pokeData = 
+
 // generateTableRows();
 
 resetButton.addEventListener('click', () => {
@@ -17,19 +16,34 @@ var ctx = document.getElementById('myChart').getContext('2d');
 // eslint-disable-next-line no-undef
 var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'bar',
+    type: 'horizontalBar',
 
     // The data for our dataset
     data: {
         labels: makePropertyArray(pokeStats, 'name'),
         datasets: [{
+            stack: 'caught',
             label: 'Number Caught',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
             data: makePropertyArray(pokeStats, 'caught')
-        }]
+        },
+        {
+            stack: 'seen',
+            label: 'Number Seen',
+            backgroundColor: 'blue',
+            borderColor: 'rgb(255, 99, 132)',
+            data: makePropertyArray(pokeStats, 'seen')
+        }
+        ]
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+        scales: {
+            xAxes: [{
+                stacked: true
+            }]
+        }
+    }
 });
