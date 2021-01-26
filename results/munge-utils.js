35bc06b17734d0ab91649769a396a1c25b350e34
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-26 12:09:47 -0800

    Add chart.js to results page, add basic chart

--- results/munge-utils.js
+++ results/munge-utils.js
@@ -1,10 +1,4 @@
-// export function makeSeenArray(arrayOfObjects) {
-//     const seenArray = [];
-//     for (let item of arrayOfObjects) {
-//         seenArray.push(item.seen);
-//     }
-//     return seenArray;
-// }
+// combined the seen, caught, and label munging into one function with a paramter to indicate which property should be accessed
 
 export function makePropertyArray(arrayOfObjects, propertyString) {
     const propertyArray = [];
@@ -14,10 +8,18 @@ export function makePropertyArray(arrayOfObjects, propertyString) {
     return propertyArray;
 }
 
-export function makeLabelArray(arrayOfObjects) {
-    const labelArray = [];
-    for (let item of arrayOfObjects) {
-        labelArray.push(item.pokemon); // or whatever the name property is called in that array
-    }
-    return labelArray;
-}
+// export function makeSeenArray(arrayOfObjects) {
+//     const seenArray = [];
+//     for (let item of arrayOfObjects) {
+//         seenArray.push(item.seen);
+//     }
+//     return seenArray;
+// }
+
+// export function makeLabelArray(arrayOfObjects) {
+//     const labelArray = [];
+//     for (let item of arrayOfObjects) {
+//         labelArray.push(item.pokemon); // or whatever the name property is called in that array
+//     }
+//     return labelArray;
+// }
--- results/results.js
+++ results/results.js
@@ -1,11 +1,35 @@
-import { generateTableRows } from './render-results-utils.js';
-import { clearStats } from '../data-utils.js';
+// import { generateTableRows } from './render-results-utils.js';
+import { clearStats, getStats } from '../data-utils.js';
+import { makePropertyArray } from './munge-utils.js';
+import pokemon from '../data/data.js';
 
 const resetButton = document.getElementById('reset-button');
-
-generateTableRows();
+const pokeStats = getStats();
+const pokeData = 
+// generateTableRows();
 
 resetButton.addEventListener('click', () => {
     clearStats();
     window.location = '../';
+});
+
+var ctx = document.getElementById('myChart').getContext('2d');
+// eslint-disable-next-line no-undef
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: makePropertyArray(pokeStats, 'name'),
+        datasets: [{
+            label: 'Number Caught',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: makePropertyArray(pokeStats, 'caught')
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
 });
