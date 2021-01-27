eeebc12e279c30ff845507077f8b87092dc682f1
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-26 20:48:13 -0800

    Add persistent session storage to localStorage

--- results/results.js
+++ results/results.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import { basicChart } from './result-charts.js';
-import { clearStats } from '../data-utils.js';
+import { getStats, clearStats, setAndUpdateAllTime } from '../data-utils.js';
 
+const sessionStats = getStats();
+const allTimeStats = setAndUpdateAllTime(sessionStats);
 const resetButton = document.getElementById('reset-button');
 
 // generateTableRows();
@@ -10,7 +12,3 @@ resetButton.addEventListener('click', () => {
     clearStats();
     window.location = '../';
 });
-
-
-
-
